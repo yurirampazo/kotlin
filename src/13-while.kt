dce4469403f50bf4bf2257b9dd4b6c6d2926c031
yuri <ymrampazo@gmail.com>
2024-12-05 21:09:27 -0300

    While

---
+++ src/13-while.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+  var i = 0
+
+  while (i < 100) {
+    println("$i")
+    i++
+  }
+
+  while (i > 0) {
+    println(i)
+    i--
+  }
+
+  val str = "kotlin"
+  var j = 0
+  while (j < str.length) {
+    print("${str[j]}")
+    j++
+  }
+
+
+
+}
+
+
