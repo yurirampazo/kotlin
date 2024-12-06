c53267778ea673038be91dc64e494559ca6851a5
yuri <ymrampazo@gmail.com>
2024-12-06 16:08:24 -0300

    do while, break and continue

---
+++ src/14-break.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+  var i = 0
+  while (i < 31) {
+    println(i)
+
+    if (i == 12) {
+      println("Cont value is $i, breaking loop")
+      break
+    }
+    i++
+  }
+
+  i = 0
+  while (i < 31) {
+    if (i < 24) {
+      i++
+      println("Continuing $i")
+      continue
+    } else {
+      break
+    }
+  }
+}
+
+
---
+++ src/14-do-while.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+  var i = 0
+  do {
+    println(i)
+    i++
+  } while (i < 11)
+}
+
+
