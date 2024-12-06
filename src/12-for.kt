c0bee2fe2bf88ea5105bf85def6969cc408d050a
yuri <ymrampazo@gmail.com>
2024-12-05 21:00:44 -0300

    For

---
+++ src/12-for.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+  for (i in 1..10) {
+    println(i)
+  }
+
+  for (i in 1..20 step 2) {
+    println(i)
+  }
+
+  for (i in 20 downTo 0) {
+    println(i)
+  }
+
+
+  val str = "kotlin is the best!"
+  for (char in str) {
+    println("$char")
+  }
+
+}
+
+
