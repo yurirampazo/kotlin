e34085a4e5bad1a876c578e20e213ef1bc471435
yuri <ymrampazo@gmail.com>
2024-12-07 16:32:29 -0300

    Naming params

---
+++ src/_2_functions/20-FunctionsDefault.kt
@@ -0,0 +1,17 @@
+package _2_functions
+
+fun main() {
+  defineAddress(
+    city = "SAO PAULO",
+    street = "AVENIDA PAULISTA",
+    state = "SP",
+    zipCode = "04144000"
+  )
+}
+
+
+fun defineAddress(street: String, city: String, state: String, zipCode: String, number: Int = 0) {
+
+}
+
+
