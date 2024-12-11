ce6bfccc9e75ac36eb7f7b67f3b92872aa8246a0
yuri <ymrampazo@gmail.com>
2024-12-10 21:50:04 -0300

    Constructors

--- src/_6_oop/Person.kt
+++ src/_6_oop/Person.kt
@@ -3,6 +3,16 @@ package _6_oop
 class Person(val birthYear: Int, var name: String) {
 
   var eyesColor: String = ""
+  private var documentNumber: String ? = null
+
+  constructor(birthYear: Int, name: String, eyesColor: String, documentNumber: String) : this(birthYear, name) {
+    //Better for validations
+    if (documentNumber.isEmpty()) {
+      this.documentNumber = ""
+    }
+    this.documentNumber = documentNumber
+  }
+
 
   fun sleep() {
     println("Good night!")
@@ -11,11 +21,18 @@ class Person(val birthYear: Int, var name: String) {
   fun wakeUp() = println("Good Morning!")
 }
 
+class Empty private constructor()
 
 fun main() {
   val person: Person = Person(1995, "yuri")
+  val person2: Person = Person(1995, "jana", "brown", "123")
+  val person3: Person = Person(1968, "katia", "green", "123")
   println(person.name)
   person.sleep()
   person.wakeUp()
   person.eyesColor = "Brown"
+
+
+//  Empty()
+//  Nothing()
 }
